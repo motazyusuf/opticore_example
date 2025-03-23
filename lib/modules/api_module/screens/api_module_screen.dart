8b36472d99560b149d95a07f87ac77d5eeebdbd2
Motaz <motazyusuf@gmail.com>
2025-03-23 22:47:17 +0200

    -Refactored into custom widget

--- lib/modules/api_module/screens/api_module_screen.dart
+++ lib/modules/api_module/screens/api_module_screen.dart
@@ -17,64 +17,13 @@ class ApiModuleScreenState
   Widget buildWidget(BuildContext context, RenderDataState state) {
     if (state is ProductsLoaded) {
       return Scaffold(
-        appBar: AppBar(title: Text("Products")),
         body: Column(
           children: [
             Expanded(
               child: ListView.builder(
                 itemCount: state.products.products.length,
                 itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(5),
-                    margin: EdgeInsets.all(10),
-                    height: 500,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.white,
-                      // image: DecorationImage(
-                      //   image: NetworkImage(
-                      //     state.products.products[index].imageURL,
-                      //   ),
-                      // ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 6,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.network(
-                            state.products.products[index].imageURL,
-                          ),
-                          Text(
-                            state.products.products[index].title,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            '\$${state.products.products[index].price}',
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+                  return Item(product: state.products.products[index]);
                 },
               ),
             ),
---
+++ lib/modules/api_module/screens/widgets/item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../products_model.dart';
+
+class Item extends StatelessWidget {
+  Item({super.key, required this.product});
+
+  Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.all(10),
+      height: 500,
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        // image: DecorationImage(
+        //   image: NetworkImage(
+        //     state.products.products[index].imageURL,
+        //   ),
+        // ),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 6, spreadRadius: 2),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(product.imageURL),
+            Text(
+              product.title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '\$${product.price}',
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
